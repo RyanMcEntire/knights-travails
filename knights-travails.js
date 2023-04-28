64d3c580dacb417c8c9c0deeef694a5f5e842746
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-04-27 21:23:27 -0600

    add bst file and possible moves

--- knights-travails.js
+++ knights-travails.js
@@ -1,3 +1,20 @@
-function knightMoves(start, end) {
-  
+const Tree = require('./binary-search-tree');
+
+function knightMoves(start, end, count) {
+  if (!start) return
+   
 }
+
+// axis will be [x, y]
+// cant move off board
+
+const possibleMoves = [
+  [1, 2],
+  [2, 1],
+  [2, -1],
+  [1, -2],
+  [-1, -2],
+  [-2, -1],
+  [-2, 1],
+  [-1, 2],
+];

f936de0daa8ab399de8a64cf65c71e17e0566cba
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-05-02 23:53:21 -0600

    complete the lesson

--- knights-travails.js
+++ knights-travails.js
@@ -1,29 +1,82 @@
-const Knight = require('./knight');
-
-function createBoard() {
-  const grid = 8;
-  const board = [];
-  for (i = 0; i < grid; i++) {
-    for (j = 0; j < grid; j++) {
-      board.push([i, j]);
-    }
+class Node {
+  constructor(square) {
+    this.square = square;
+    this.children = [];
+    this.parent = null;
   }
-  return board;
 }
 
-const chessBoard = createBoard();
+class Knight {
+  constructor(currentPosition, destination) {
+    this.moves = [
+      [1, 2],
+      [2, 1],
+      [2, -1],
+      [1, -2],
+      [-1, -2],
+      [-2, -1],
+      [-2, 1],
+      [-1, 2],
+    ];
+    this.startingSquare = currentPosition;
+    this.destination = destination;
+    this.root = null;
+    this.visited = new Set();
+  }
 
-console.log(chessBoard);
+  // adds all 8 moves to currentPosition
+  getAllMoves(currentPosition) {
+    const moveArray = [];
+    this.moves.forEach((move) => {
+      const x = move[0] + currentPosition[0];
+      const y = move[1] + currentPosition[1];
+      // if (x == this.startingSquare[0] && y == this.startingSquare[1]) return;
+      if (x < 0 || x > 7 || y < 0 || y > 7) return;
+      moveArray.push([x, y]);
+    });
+    return moveArray;
+  }
 
-function knightMoves(start, end, count) {
-  if (!start) return;
+  findShortestPath() {
+    const queue = [{ node: new Node(this.startingSquare), depth: 0 }];
+
+    while (queue.length > 0) {
+      const { node: currentNode, depth } = queue.shift();
+
+      if (
+        currentNode.square[0] === this.destination[0] &&
+        currentNode.square[1] === this.destination[1]
+      ) {
+        const path = [];
+        let tempNode = currentNode;
+        while (tempNode) {
+          path.unshift(tempNode.square);
+          tempNode = tempNode.parent;
+        }
+        return path;
+      }
+
+      const moves = this.getAllMoves(currentNode.square);
+      moves.forEach((move) => {
+        const moveStr = JSON.stringify(move);
+        if (!this.visited.has(moveStr)) {
+          this.visited.add(moveStr);
+          const childNode = new Node(move);
+          childNode.parent = currentNode;
+          currentNode.children.push(childNode);
+          queue.push({ node: childNode, depth: depth + 1 });
+        }
+      });
+    }
+    // safety net lol
+    return [];
+  }
 }
 
-const ourKnight = new Knight([4, 4], [5, 4]);
+function knightMoves(startingSquare, destinationSquare) {
+  const ourKnight = new Knight(startingSquare, destinationSquare);
+  const shortestPath = ourKnight.findShortestPath();
+  return console.log('shortest path', shortestPath);
+}
 
-const knightsMoves = ourKnight.getAllMoves(ourKnight.startingSquare);
-const bestMoves = ourKnight.sortMoveCoordinates(
-  knightsMoves,
-  ourKnight.destination
-);
-console.log(ourKnight, bestMoves);
+knightMoves([4, 4], [7, 6]);
